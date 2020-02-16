8d407c8ebdffd28c9fcf48dbcdcb8037af02f545
Mauro Guardarini <mauro.guardarini@gmail.com>
2020-02-16 19:26:50 -0300

    init

---
+++ app/src/main/java/com/example/migrate/adapter/ListItensAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.migrate.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class ListItensAdapter : BaseAdapter() {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItem(position: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
